b25a5c8a117a3c6d25982b8a543e6a4457257fb6
shiyl <425324438@qq.com>
2020-09-05 16:27:08 +0800

    redis

--- redisSharedTestCase/src/main/java/com/redis/test/RedisString.java
+++ redisSharedTestCase/src/main/java/com/redis/test/RedisString.java
@@ -4,18 +4,22 @@ import com.redis.test.redis.connection.RedisConnection;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.params.SetParams;
 
-import javax.xml.ws.RequestWrapper;
-import java.io.IOException;
-
 /**
  * @author: 425324438@qq.com
- * @Date: 2020/8/30 15:51
+ * @Date: 2020/9/5 16:21
  * @Description:
  */
-public class Main {
-
+public class RedisString {
     public static void main(String[] args) {
         Jedis jedis = RedisConnection.getConnection();
+
+        /**
+         * cacheKey 缓存的key
+         * cacheValue 缓存的value
+         * SetParams.setParams().ex(10)  缓存的超时时间
+         */
+        jedis.set("cacheKey","cacheValue",SetParams.setParams().ex(10));
+
         //分布式锁的正确姿势
         jedis.set("","", SetParams.setParams().nx());
     }
