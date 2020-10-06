f02b218d335dc4a2308068da7dfb8b064971a188
shiyl <425324438@qq.com>
2020-10-06 22:03:38 +0800

    redis HyperLogLog

--- redisSharedTestCase/src/main/java/com/redis/test/key/type/RedisHyperloglogs.java
+++ redisSharedTestCase/src/main/java/com/redis/test/key/type/RedisHyperloglogs.java
@@ -16,9 +16,11 @@ public class RedisHyperloglogs {
         Jedis jedis = RedisConnection.getConnection();
         //清空 key
         jedis.del("keypf");
+        //循环写入数据
         for (int i = 0; i < 1000; i++) {
             jedis.pfadd("keypf", UUID.randomUUID().toString());
         }
+        //获取统计数据
         long pfcount = jedis.pfcount("keypf");
         System.out.println("1000次写入，count的结果="+pfcount);
     }
