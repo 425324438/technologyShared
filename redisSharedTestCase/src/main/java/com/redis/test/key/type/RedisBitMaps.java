877dd7b2b23f3023cf303b623e657f5daed95b60
shiyl <425324438@qq.com>
2020-10-18 17:27:21 +0800

    redis 地理位置

--- redisSharedTestCase/src/main/java/com/redis/test/key/type/RedisBitMaps.java
+++ redisSharedTestCase/src/main/java/com/redis/test/key/type/RedisBitMaps.java
@@ -4,31 +4,40 @@ import com.redis.test.redis.connection.RedisConnection;
 import redis.clients.jedis.BitPosParams;
 import redis.clients.jedis.Jedis;
 
+
 /**
  * @author: 425324438@qq.com
  * @Date: 2020/9/6 21:54
  * @Description:
  */
 public class RedisBitMaps {
+    static String signIn = "SignIn";
     public static void main(String[] args) {
+
         Jedis jedis = RedisConnection.getConnection();
-        jedis.setbit("bitKey",0L, false);
 
-        Boolean bitKey = jedis.getbit("bitKey", 0L);
-        System.out.println("读取 bitKey="+bitKey);
+        System.out.println("模拟生成用户30天签到记录");
+        int signCount = 0;
+        for (int i = 0; i < 30; i++) {
+            boolean bool = randomBool();
+            signCount += bool ? 1 : 0;
+            if(bool){
+                System.out.println("\r第"+i+"天签到了,SignIn="+bool);
+            }
+            jedis.setbit(signIn,(long)i,bool);
+        }
+        System.out.println("模拟签到统计天数="+signCount);
+        System.out.println("模拟生成用户30天签到记录,结束");
+        System.out.println("---- ---- ---- ---- ---- --");
 
-        //
-        jedis.bitcount("bitKey");
-
-
-        //返回字符串里面第一个被设置为1或者0的bit位。
-        // false=0,true=1
-        Long bitKey2 = jedis.bitpos("bitKey", false);
-        System.out.println("第一个出现false的位置="+bitKey2);
-
-        //获取指定范围内第一次出现false的位置
-        Long bitKey1 = jedis.bitpos("bitKey", false, new BitPosParams(0L, 10L));
-        System.out.println("在0 ~ 10 只能第一个出现false的位置="+bitKey1);
+        System.out.println("redis获取实际签到天数= "+jedis.bitcount(signIn));
+        System.out.println("第一天签到的时间= "+jedis.bitpos(signIn,true,new BitPosParams(0)));
+        System.out.println("查询第19天是否签到= "+jedis.getbit(signIn,19L));
 
     }
+
+    public static boolean randomBool(){
+        return Math.random() > 0.7;
+    }
+
 }
